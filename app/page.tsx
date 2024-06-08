745e322ef601b57beaed437509dbe829bd766d9e
dev <vipul@boostmychild.com>
2024-06-08 21:15:45 +0530

    design modificatons

--- app/page.tsx
+++ app/page.tsx
@@ -28,19 +28,29 @@ export default function Home() {
   }, [isDarkMode]);
 
   return (
-    <section id="welcomeSection" className=" flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center w-3/5 h-full ">
-        <h1 className="text-2xl font-signature text-center">
-          Hello, I am Shyam Latake, a passionate developer dedicated to crafting innovative and impactful digital experiences.  </h1>
-        <p className="text-center mt-4">
-          Dive into my projects to see the blend of creativity and technology that drives my work. Join me on a journey of innovation and excellence. Thank you for visiting and enjoy exploring!                   </p>
-      </div>
-      <span className="drop bg-black before:bg-black dark:bg-white dark:before:bg-white"></span>
+    <>
+      <section id="welcomeSection" className=" flex items-end justify-center sm:justify-end">
+        <span className="drop bg-black before:bg-black dark:bg-white dark:before:bg-white"></span>
+        <div className="flex flex-col items-center justify-center sm:items-center sm:justify-end sm:self-end w-4/6 h-full">
+          <Image src="/images/mainImage.svg" alt="Shyam Latake" width={400} height={200} className="" />
+          <h1 className="text-xl sm:w-4/5 subpixel-antialiased font-signature text-center mt-4 text-black dark:text-white">
+            Hello, I am Shyam Latake, a passionate developer dedicated to crafting innovative and impactful digital experiences.  </h1>
+          {/* <p className="text-center mt-4">
+          Dive into my projects to see the blend of creativity and technology that drives my work. Join me on a journey of innovation and excellence. Thank you for visiting and enjoy exploring!                   </p> */}
+        </div>
 
-      <div className="w-2/5 h-screen">
-        {/* <Image src="/images/hero.svg" alt="hero" layout="fill" objectFit="contain" />  */}
-      </div>
+      </section>
+      <section id="aboutSection" className="flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center w-4/6 h-full">
+          <h1 className="text-2xl subpixel-antialiased font-signature text-center mt-4 text-black dark:text-white">
+            About Me
+          </h1>
+          <p className="text-center mt-4">
+            I am a passionate developer dedicated to crafting innovative and impactful digital experiences. Dive into my projects to see the blend of creativity and technology that drives my work. Join me on a journey of innovation and excellence. Thank you for visiting and enjoy exploring!
+          </p>
+        </div>
+      </section>
+    </>
 
-    </section>
   );
 }
