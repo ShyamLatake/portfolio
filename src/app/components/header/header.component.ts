4b3a7efc6101bf56078d00ff7df3365c6b714681
= <=>
2025-08-12 16:57:49 +0530

    responsviness added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,8 +5,22 @@ import { NavbarDirective } from '../../shared/directives/navbar.directive';
   standalone: true,
   imports: [NavbarDirective],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+ isMenuOpen = false;
+  activeSection = 'home';
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  setActive(section: string) {
+    this.activeSection = section;
+    this.closeMenu();
+  }
 }
